e04524032b99bce5f758b4d5047e2d7a406bafab
Bhavnit Singh Bajwa <bajwabsingh@yahoo.com>
2013-11-13 13:18:22 +0000

    - Implemented     - SystemInterface.XML.Serialization.IXmlSerializer         - IXmlSerializer.XmlSerializerInstance         - IXmlSerializer.Deserialize(IFileStream stream)         - NewXMLSerializer(type, type[])
    - Removed SystemWrapper.Samples/bin folder from repo

---
+++ SystemWrapper4/SystemInterface/XML/Serialization/IXmlSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+using SystemInterface.IO;
+
+namespace SystemInterface.XML.Serialization
+{
+    public interface IXmlSerializer
+    {
+        /// <summary>
+        /// Creates a new IXmlSerializer object by passing the constructor.
+        /// </summary>
+        /// <param name="type">The Type which is to be serialized</param>
+        /// <param name="otherTypes">Any other types used within the Type</param>
+        void NewXMLSerializer(Type type, Type[] otherTypes);
+
+        /// <summary>
+        /// Gets the underline XmlSerializer instance.
+        /// </summary>
+        XmlSerializer XmlSerializerInstance { get; }
+
+        /// <summary>
+        /// Deserializes the XML document contained by the specified System.IO.Stream.
+        /// </summary>
+        /// <param name="stream">The System.IO.Stream that contains the XML Document to deserialize.</param>
+        object Deserialize(IFileStream stream);
+    }
+}
---
+++ SystemWrapper4/SystemWrapper/XML/Serialization/XmlSerializerWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+using SystemInterface.IO;
+using SystemInterface.XML.Serialization;
+
+namespace SystemWrapper.XML.Serialization
+{
+    public class XmlSerializerWrapper : IXmlSerializer
+    {
+        public XmlSerializer XmlSerializerInstance { get; private set; }
+
+        public XmlSerializerWrapper()
+        {
+            XmlSerializerInstance = null;
+        }
+
+        public void NewXMLSerializer(Type type, Type[] otherTypes)
+        {
+            XmlSerializerInstance = new XmlSerializer(type, otherTypes);
+        }
+
+        public object Deserialize(IFileStream stream)
+        {
+            return XmlSerializerInstance.Deserialize(stream.FileStreamInstance);
+        }
+    }
+}
