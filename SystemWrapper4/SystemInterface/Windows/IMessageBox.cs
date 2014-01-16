b107effcb6ea35f629c561d98508213c00cc9318
Bhavnit Singh Bajwa <bajwabsingh@yahoo.com>
2014-01-16 06:22:27 +0000

    - Added Support for System.Windows.IMessageBox

---
+++ SystemWrapper4/SystemInterface/Windows/IMessageBox.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SystemInterface.Windows
+{
+    /// <summary>
+    /// Displays a message box.
+    /// </summary>
+    public interface IMessageBox
+    {
+        /// <summary>
+        /// Displays a message box that has a message and that returns a result.
+        /// </summary>
+        /// <param name="messageBoxText">A System.String that specifies the text to display.</param>
+        /// <returns>A System.Windows.MessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        MessageBoxResult Show(string messageBoxText);
+
+        /// <summary>
+        /// Displays a message box that has a message and title bar caption; and that
+        /// returns a result.
+        /// </summary>
+        /// <param name="messageBoxText">A System.String that specifies the text to display.</param>
+        /// <param name="caption">A System.String that specifies the title bar caption to display.</param>
+        /// <returns>A System.Windows.MessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        MessageBoxResult Show(string messageBoxText, string caption);
+        
+        /// <summary>
+        /// Displays a message box that has a message, title bar caption, and button;
+        /// and that returns a result.
+        /// </summary>
+        /// <param name="messageBoxText">A System.String that specifies the text to display.</param>
+        /// <param name="caption">A System.String that specifies the title bar caption to display.</param>
+        /// <param name="button">A System.Windows.MessageBoxButton value that specifies which button or buttons to display.</param>
+        /// <returns>A System.Windows.MessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button);
+        
+        /// <summary>
+        /// Displays a message box that has a message, title bar caption, button, and
+        /// icon; and that returns a result.
+        /// </summary>
+        /// <param name="messageBoxText">A System.String that specifies the text to display.</param>
+        /// <param name="caption">A System.String that specifies the title bar caption to display.</param>
+        /// <param name="button">A System.Windows.MessageBoxButton value that specifies which button or buttons to display.</param>
+        /// <param name="icon">A System.Windows.MessageBoxImage value that specifies the icon to display.</param>
+        /// <returns>A System.Windows.MessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon);
+
+        /// <summary>
+        /// Displays a message box that has a message, title bar caption, button, and
+        /// icon; and that accepts a default message box result and returns a result.
+        /// </summary>
+        /// <param name="messageBoxText">A System.String that specifies the text to display.</param>
+        /// <param name="caption">A System.String that specifies the title bar caption to display.</param>
+        /// <param name="button">A System.Windows.MessageBoxButton value that specifies which button or buttons to display.</param>
+        /// <param name="icon">A System.Windows.MessageBoxImage value that specifies the icon to display.</param>
+        /// <param name="defaultResult">A System.Windows.MessageBoxResult value that specifies the default result of the message box.</param>
+        /// <returns>A System.Windows.MessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult);
+                
+        /// <summary>
+        /// Displays a message box that has a message, title bar caption, button, and
+        /// icon; and that accepts a default message box result, complies with the specified
+        /// options, and returns a result.
+        /// </summary>
+        /// <param name="messageBoxText">A System.String that specifies the text to display.</param>
+        /// <param name="caption">A System.String that specifies the title bar caption to display.</param>
+        /// <param name="button">A System.Windows.MessageBoxButton value that specifies which button or buttons to display.</param>
+        /// <param name="icon">A System.Windows.MessageBoxImage value that specifies the icon to display.</param>
+        /// <param name="defaultResult">A System.Windows.MessageBoxResult value that specifies the default result of the message box.</param>
+        /// <param name="options">A System.Windows.MessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A System.Windows.MessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult, MessageBoxOptions options);
+    }
+}
---
+++ SystemWrapper4/SystemWrapper/Windows/MessageBoxWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+using SystemInterface.Windows;
+
+namespace SystemWrapper.Windows
+{
+    /// <summary>
+    /// Wrapper for <see cref="T:System.Windows.MessageBox"/> class.
+    /// </summary>
+    public class MessageBoxWrapper : IMessageBox
+    {
+        /// <inheritdoc />
+        public MessageBoxResult Show(string messageBoxText)
+        {
+            return MessageBox.Show(messageBoxText);
+        }
+
+        /// <inheritdoc />
+        public MessageBoxResult Show(string messageBoxText, string caption)
+        {
+            return MessageBox.Show(messageBoxText, caption);
+        }
+
+        /// <inheritdoc />
+        public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button)
+        {
+            return MessageBox.Show(messageBoxText, caption, button);
+        }
+
+        /// <inheritdoc />
+        public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon)
+        {
+            return MessageBox.Show(messageBoxText, caption, button, icon);
+        }
+
+        /// <inheritdoc />
+        public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult)
+        {
+            return MessageBox.Show(messageBoxText, caption, button, icon, defaultResult);
+        }
+
+        /// <inheritdoc />
+        public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon, MessageBoxResult defaultResult, MessageBoxOptions options)
+        {
+            return MessageBox.Show(messageBoxText, caption, button, icon, defaultResult, options);
+        }
+    }
+}
